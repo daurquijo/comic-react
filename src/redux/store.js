53d70e8d5b2d902044f4e4b767af1eeef6b1a0e3
durquijo <davidurquijoh@gmail.com>
2024-06-23 11:48:53 -0500

    update redux detools dependencie

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { thunk } from 'redux-thunk';
 import reducer from './reducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 const middleware = [thunk];
 const store = createStore(
